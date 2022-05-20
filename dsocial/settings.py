ad5c48e2a8ca5a99d27a57fb43655d145089d675
jeancharlles <jean_charlles@hotmail.com>
2022-05-19 22:56:42 -0300

    Adicionado script facebook

--- dsocial/settings.py
+++ dsocial/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY = 'django-insecure-6bf^*-w^p*a!&szfk%(ez1x0y0h4j#ka4wi@2oixzbbk+^(jrw'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
